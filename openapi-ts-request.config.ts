3d7dbb827ee0bfe24ab00b45696e13f806a53e46
故城 <1055120207@qq.com>
2025-10-20 10:24:20 +0800

    fix: fix bug #536 (#538)
    * fix: fix bug #536
    
    * chore: clean code

--- openapi-ts-request.config.ts
+++ openapi-ts-request.config.ts
@@ -1,6 +1,8 @@
+import { type GenerateServiceProps } from './src/index';
+
 export default [
   {
     schemaPath: 'http://petstore.swagger.io/v2/swagger.json',
     serversPath: './apis/app',
   },
-];
+] as GenerateServiceProps[];
